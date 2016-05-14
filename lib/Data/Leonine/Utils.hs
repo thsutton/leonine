d03c8b7d6f16abce4657b0119d272b3aa2c28725
Thomas Sutton <me@thomas-sutton.id.au>
2016-05-14 20:11:20 +1000

    Helper to merge sorted, indexed vectors

--- lib/Data/Leonine/Utils.hs
+++ lib/Data/Leonine/Utils.hs
@@ -13,6 +13,14 @@ import qualified Data.Vector.Generic         as V
 import qualified Data.Vector.Generic.Mutable as V (write)
 import           Data.Word
 
+-- $setup
+-- >>> :set -XScopedTypeVariables
+-- >>> import Test.QuickCheck
+-- >>> import qualified Data.List as L
+-- >>> import qualified Data.Vector as VC
+-- >>> let sample1 = VC.fromList [('a',1),('c',3),('d',4)] :: VC.Vector (Char, Int)
+-- >>> let sample2 = VC.fromList [('a',9),('b',10),('e',10)] :: VC.Vector (Char, Int)
+
 -- * Words
 
 type Index = Word16
@@ -72,20 +80,20 @@ searchBoundsOn index v ix = loop
 -- The supplied function can insert, modify, or delete the element
 -- which corresponds to the supplied index:
 --
--- >>> Data.Vector.toList $ modify fst 'c' (Data.Vector.fromList [('a',1),('c',3),('d',4)]) (fmap (fmap (10+)))
+-- >>> Data.Vector.toList $ modify fst 'c' sample1 (fmap (fmap (10+)))
 -- [('a',1),('c',13),('d',4)]
 --
--- >>> Data.Vector.toList $ modify fst 'b' (Data.Vector.fromList [('a',1),('c',3),('d',4)]) (const $ Just ('b',9))
+-- >>> Data.Vector.toList $ modify fst 'b' sample1 (const $ Just ('b',9))
 -- [('a',1),('b',9),('c',3),('d',4)]
 --
--- >>> Data.Vector.toList $ modify fst 'a' (Data.Vector.fromList [('a',1),('c',3),('d',4)]) (const Nothing)
+-- >>> Data.Vector.toList $ modify fst 'a' sample1 (const Nothing)
 -- [('c',3),('d',4)]
--- >>> Data.Vector.toList $ modify fst 'c' (Data.Vector.fromList [('a',1),('c',3),('d',4)]) (const Nothing)
+-- >>> Data.Vector.toList $ modify fst 'c' sample1 (const Nothing)
 -- [('a',1),('d',4)]
--- >>> Data.Vector.toList $ modify fst 'd' (Data.Vector.fromList [('a',1),('c',3),('d',4)]) (const Nothing)
+-- >>> Data.Vector.toList $ modify fst 'd' sample1 (const Nothing)
 -- [('a',1),('c',3)]
 --
--- >>> Data.Vector.toList $ modify fst 'b' (Data.Vector.fromList [('a',1),('c',3),('d',4)]) (const Nothing)
+-- >>> Data.Vector.toList $ modify fst 'b' sample1 (const Nothing)
 -- [('a',1),('c',3),('d',4)]
 modify :: (Ord b, Vector v a)
        => (a -> b) -- ^ Index an element.
@@ -106,3 +114,29 @@ modify index ix v f =
       Just e' -> V.modify (\v -> V.write v i e') v
       Nothing -> let i' = i + 1
                 in (V.slice 0 i v) V.++ (V.slice i' (len - i') v)
+
+-- | Merge two 'Vector's, sorted according to some index type 'b'.
+--
+-- Elements with the same index 'b' will be merged with the supplied function.
+--
+-- >>> let mergeOp (k, s1) (_, s2) = (k, s1 + s2)
+-- >>> Data.Vector.toList $ mergeWith fst mergeOp sample1 sample2
+-- [('a',10),('b',10),('c',3),('d',4),('e',10)]
+mergeWith
+    :: (Ord b, Vector v a)
+    => (a -> b) -- ^ Element index.
+    -> (a -> a -> a) -- ^ Merge.
+    -> v a
+    -> v a
+    -> v a
+mergeWith index merge = loop
+  where
+    loop v1 v2
+      | V.null v1 = v2
+      | V.null v2 = v1
+      | otherwise = let e1  = V.head v1
+                        e2  = V.head v2
+                    in case compare (index e1) (index e2) of
+                            LT -> e1 `V.cons` loop (V.tail v1) v2
+                            EQ -> (merge e1 e2) `V.cons` loop (V.tail v1) (V.tail v2)
+                            GT -> e2 `V.cons` loop v1 (V.tail v2)
